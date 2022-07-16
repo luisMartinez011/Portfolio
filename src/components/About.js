63ef8da00f341bd53ab691852efb75a59fda8019
Your Name <luis.martinez33@protonmail.com>
2022-07-15 19:29:20 -0500

    project arreglado

--- src/components/About.js
+++ src/components/About.js
@@ -3,22 +3,32 @@ import React from 'react'
 const About = () => {
   return (
     <article className='xl:ml-80 h-screen  
-    w-full pt-20 p-4 sm:p-28 sm:pt-20 font-mono '>
-      <h1 className='text-5xl'>My skills</h1>
-      <section className='flex flex-wrap w-full mt-5 justify-between'>
+    w-full 
+    pt-20 p-4 sm:p-28 sm:pt-20 font-mono '>
+      <h1 className='text-5xl'>About me</h1>
+      <div>
+        <p className='text-lg'>Hi, my name is Luis Martinez. I'm a fullstack developer with a passion for lifelong learning.
 
-        <div className='mt-10 w-full 2xl:w-5/12'>
+          I believe that technological advancements have improved our lives and made the world a better place. The Web has connected our whole world and enabled everyday people to think globally.
+
+          I became a developer because I want to create, contribute and be a part of this amazing connection. </p>
+      </div>
+      <hr className='mt-5 divide-red-600' />
+      <h1 className='text-5xl mt-5'>My skills</h1>
+      <section className='flex flex-col w-full mt-5 justify-between'>
+
+        <div className='mt-10 w-full 2xl:w-9/12'>
           <h1 className='text-2xl'>Languages:</h1>
-          <div className='flex items-center w-full h-24 bg-red-500/50
+          <div className='flex items-center w-full h-28 bg-red-500/50
           text-lg mt-5'>
             <div className='w-5 h-full bg-red-600'></div>
             <p className='ml-5'>JavaScript, TypeScript, C#, HTML/CSS</p>
           </div>
         </div>
 
-        <div className='mt-10 w-full 2xl:w-5/12'>
+        <div className='mt-10 w-full 2xl:w-9/12'>
           <h1 className='text-2xl'>Frameworks:</h1>
-          <div className='flex items-center w-full h-24 bg-red-500/50
+          <div className='flex items-center w-full h-28 bg-red-500/50
           text-lg mt-5'>
             <div className='w-5 h-full bg-red-600'></div>
             <div className='ml-5'>
@@ -34,30 +44,30 @@ const About = () => {
           </div>
         </div>
 
-        <div className='mt-10 w-full 2xl:w-5/12'>
-          <h1 className='text-2xl'>Databses:</h1>
-          <div className='flex items-center w-full h-24 bg-red-500/50
+        <div className='mt-10 w-full 2xl:w-9/12'>
+          <h1 className='text-2xl'>Databases:</h1>
+          <div className='flex items-center w-full h-28 bg-red-500/50
           text-lg mt-5'>
             <div className='w-5 h-full bg-red-600'></div>
             <p className='pl-5'>PostgreSQL, MongoDB</p>
           </div>
         </div>
 
-        <div className='mt-10 w-full 2xl:w-5/12'>
+        <div className='mt-10 w-full 2xl:w-9/12'>
           <h1 className='text-2xl'>Devops:</h1>
-          <div className='flex items-center w-full h-24 bg-red-500/50
+          <div className='flex items-center w-full h-28 bg-red-500/50
           text-lg mt-5'>
             <div className='w-5 h-full bg-red-600'></div>
             <p className='pl-5'>Azure, Docker</p>
           </div>
         </div>
 
-        <div className='mt-10 w-full 2xl:w-5/12'>
+        <div className='mt-10 w-full 2xl:w-9/12'>
           <h1 className='text-2xl'>Other:</h1>
-          <div className='flex items-center w-full h-24 bg-red-500/50
+          <div className='flex items-center w-full h-28 bg-red-500/50
           text-lg mt-5'>
             <div className='w-5 h-full bg-red-600'></div>
-            <p className='pl-5'>Git, Github, Docker, Heroku</p>
+            <p className='pl-5'>Git, Github, React Native, Linux </p>
           </div>
         </div>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,36 +17,33 @@ const Navbar = () => {
 
   return (
     <nav className={'fixed  w-64 md:w-72 xl:w-80 h-full m-0 p-0 font-mono '
-    + ( (toggle)? "z-20" : "") }>
+      + ((toggle) ? "z-20" : "")}>
       <MdMenu className={'absolute top-5 -z-10 xl:hidden bg-red-400 w-14 h-14 ' + ((toggle) ? "left-full" : "left-0")}
         onClick={handleNavbarToggle}></MdMenu>
 
-      <div className={"flex flex-col h-screen  bg-red-400 relative xl:static divide-y-4 divide-solid text-zinc-50 text-xl" 
-      + ( (toggle)? "" : " right-full ") }>
+      <div className={"flex flex-col h-screen  bg-red-400 relative xl:static divide-y-4 divide-solid text-zinc-50 text-xl"
+        + ((toggle) ? "" : " right-full ")}>
 
         <div className='flex h-64 md:h-72 xl:h-80 items-center 
         justify-center'>
-        <NavLink to="/" className=''>
-          <img src="https://1.bp.blogspot.com/-Muc21xaQjYg/XluhYO6f6vI/AAAAAAAAPJw/T51WnNCctz4Vxn_9REzJuftkznfuRGKUgCLcBGAsYHQ/s1600/whatsapp%2Bprofile%2Bpic%2B%25281%2529.jpg"
-            alt="Profile" className=' rounded-full border-4 border-red-400 w-40 md:w-52 xl:w-60'></img>
-        </NavLink>
+          <NavLink to="/" className=''>
+            <img src="https://1.bp.blogspot.com/-Muc21xaQjYg/XluhYO6f6vI/AAAAAAAAPJw/T51WnNCctz4Vxn_9REzJuftkznfuRGKUgCLcBGAsYHQ/s1600/whatsapp%2Bprofile%2Bpic%2B%25281%2529.jpg"
+              alt="Profile" className=' rounded-full border-4 border-red-400 w-40 md:w-52 xl:w-60'></img>
+          </NavLink>
         </div>
-        
+
 
         <div className='divide-y-4 divide-solid
         flex flex-col justify-center h-full w-full '>
-          <NavLink to="/" className="w-full hover:bg-red-600 active:bg-red-700 p-10" style={linkStyle}>Home</NavLink>
-          <NavLink to="/about" className="w-full hover:bg-red-600 active:bg-red-700 p-10" style={linkStyle}>About me</NavLink>
-          <NavLink to="/projects" className="w-full hover:bg-red-600 active:bg-red-700 p-10" style={linkStyle}>Projects</NavLink>
-          <NavLink to="contact" className="w-full hover:bg-red-600 active:bg-red-700 p-10" style={linkStyle}>Contact me</NavLink>
+          <NavLink to="/" className="w-full hover:bg-red-600 active:bg-red-700 p-10" style={linkStyle} onClick={handleNavbarToggle}>Home</NavLink>
+          <NavLink to="/about" className="w-full hover:bg-red-600 active:bg-red-700 p-10" style={linkStyle} onClick={handleNavbarToggle}>About me</NavLink>
+          <NavLink to="/projects" className="w-full hover:bg-red-600 active:bg-red-700 p-10" style={linkStyle} onClick={handleNavbarToggle}>Projects</NavLink>
+          <NavLink to="contact" className="w-full hover:bg-red-600 active:bg-red-700 p-10" style={linkStyle} onClick={handleNavbarToggle}>Contact me</NavLink>
         </div>
 
       </div>
 
     </nav>
-
-
-
   )
 }
 
